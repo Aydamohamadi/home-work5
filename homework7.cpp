5b9036fc4bbc622d4d90f9df77b2717f6e5d0a2b
Aydamohamadi <152727187+Aydamohamadi@users.noreply.github.com>
2023-12-08 00:19:47 -0800

    Add files via upload

---
+++ homework7.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {int m,n,t;
+cout<<"please inter a value for m";
+cin>>m;
+cout<<"please inter a value for n";
+cin>>n;
+
+while (m!=0){t=m%n; n=m; m=n;
+}
+cout<<n;
+
+      
+	  
+	  
+	  
+	  
+	  
+	  
+	  return 0;
+}
